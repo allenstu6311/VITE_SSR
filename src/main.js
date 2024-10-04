f2d7fbc37b50718d5b304c3ed2d4aec552c4a698
allen <>
2024-10-05 00:30:17 +0800

    ssr build

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// import { createApp } from 'vue'
+import "./style.css";
+// import App from './App.vue'
+
+// createApp(App).mount('#app')
+
+import { createSSRApp } from "vue";
+import App from "./App.vue";
+
+// SSR requires a fresh app instance per request, therefore we export a function
+// that creates a fresh app instance. If using Vuex, we'd also be creating a
+// fresh store here.
+export function createApp() {
+  const app = createSSRApp(App);
+  return { app };
+}
