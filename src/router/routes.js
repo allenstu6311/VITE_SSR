e1de4751f973edaf02bcd7c433e9505d9fe99def
allen <>
2024-10-06 23:29:55 +0800

    ssg successs

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,4 +1,23 @@
 export const routes = [
   { path: "/", name: "home" },
   { path: "/about", name: "about" },
+  {
+    path: "/trade",
+    name: "trade",
+    ssg: false,
+    children: [
+      {
+        path: "/ex",
+        name: "ex",
+      },
+      {
+        path: "/co",
+        name: "co",
+      },
+      {
+        path: "/otc",
+        name: "otc",
+      },
+    ],
+  },
 ];
--- ssg.js
+++ ssg.js
@@ -2,40 +2,7 @@ import readline from "readline";
 import { render } from "./dist/server/entry-server.js";
 import fs from "fs";
 import path from "path";
-import { routes } from "./src/router/routes.js";
-
-/**
- * 生成檔案
- * @param {String} pathVal 路徑(ex:/about,/test...)
- */
-async function createFile(pathVal) {
-  const template = fs.readFileSync(
-    path.resolve("dist/client/index.html"),
-    "utf-8"
-  );
-
-  const { html } = await render(`${pathVal}`);
-  if (!html) {
-    console.log("查無此路徑");
-  }
-  const responseHtml = template.replace("<!--ssr-outlet-->", html);
-
-  const outputPath = `dist/ssg${pathVal}`;
-
-  // 生成檔案名稱
-  const filePath = path.join(
-    outputPath,
-    `${pathVal.substring(1) || "index"}.html`
-  );
-
-  // 創建ssg資料夾
-  if (!fs.existsSync(outputPath)) {
-    fs.mkdirSync(outputPath, { recursive: true });
-  }
-
-  // 寫入內容
-  fs.writeFileSync(filePath, responseHtml);
-}
+import { routes as baseRoutes } from "./src/router/routes.js";
 
 // 創建readline實體
 const rl = readline.createInterface({
@@ -52,9 +19,7 @@ services.forEach((service, index) => {
 rl.question("請選擇操作 (1 或 2):", (answer) => {
   const choice = parseInt(answer);
   if (choice === 1) {
-    for (let i = 0; i < routes.length; i++) {
-      createFile(routes[i].path);
-    }
+    recursionRoute(baseRoutes);
     rl.close();
   } else {
     rl.question("請輸入路徑名稱:", (pathVal) => {
@@ -63,3 +28,52 @@ rl.question("請選擇操作 (1 或 2):", (answer) => {
     });
   }
 });
+
+/**
+ * 生成所有檔案
+ * @param {Array} routes 當前路由
+ */
+function recursionRoute(routes) {
+  for (let i = 0; i < routes.length; i++) {
+    // ssg false
+    if (routes[i].ssg === false) continue;
+    const child = routes[i].children;
+    if (child) {
+      recursionRoute(child);
+    } else {
+      createFile(routes[i].path);
+    }
+  }
+}
+
+/**
+ * 生成檔案
+ * @param {String} pathVal 路徑(ex:/about,/test...)
+ */
+async function createFile(pathVal) {
+  const template = fs.readFileSync(
+    path.resolve("dist/client/index.html"),
+    "utf-8"
+  );
+
+  const { html } = await render(`${pathVal}`);
+  if (!html) {
+    console.log("查無此路徑");
+  }
+  const responseHtml = template.replace("<!--ssr-outlet-->", html);
+  const outputPath = `dist/ssg${pathVal}`;
+
+  // 生成檔案名稱
+  const filePath = path.join(
+    outputPath,
+    `${pathVal.substring(1) || "index"}.html`
+  );
+
+  // 創建ssg資料夾
+  if (!fs.existsSync(outputPath)) {
+    fs.mkdirSync(outputPath, { recursive: true });
+  }
+
+  // 寫入內容
+  fs.writeFileSync(filePath, responseHtml);
+}
